283545e690b83f1a6f036e12ff5123bf8905c27c
Emil Schlichter <124031667+emsc0001@users.noreply.github.com>
2024-08-22 09:02:07 +0200

    Initial commit

---
+++ src/main/java/com/example/eksamen_backend/EksamenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamenBackendApplication.class, args);
+    }
+
+}
